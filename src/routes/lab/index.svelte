a118d13eb0a82df34871dac41eeb9e37b617be54
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2022-02-05 07:35:42 -0800

    Added first lab page

--- src/routes/lab/index.svelte
+++ src/routes/lab/index.svelte
@@ -1,5 +1,9 @@
 <svelte:head>
-	<title>OK Arcadia: The Lab</title>
+  <title>OK Arcadia: The Lab</title>
 </svelte:head>
 
 <h1>The Lab</h1>
+
+<ul>
+  <li><a href="/lab/001">Experiment #001: Hello World</a></li>
+</ul>
