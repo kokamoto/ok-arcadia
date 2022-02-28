73e49db24abaaaaa253bbe4fa3c65fe6f0a8b972
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2022-02-27 21:36:37 -0800

    Adding LabLayout template to first experiment page

--- src/routes/lab/001.svelte
+++ src/routes/lab/001.svelte
@@ -1,20 +1,22 @@
 <script>
+  import LabLayout from '../../lib/components/layouts/LabLayout.svelte';
   let name = "World";
 </script>
 
-<h1 class="experiment">Experiment #001</h1>
-<h2>Hello World!</h2>
-
-<section class="introduction">
-  <p>The first experiment focuses on developing a template/layout for future lab experiments.</p>
-</section>
-
-<section class="input">
-  <h3>Input</h3>
-  <input bind:value={name}/>
-</section>
-
-<section class="output">
-  <h3>Output</h3>
-  <p>Hello {name}!</p>
-</section>
+<LabLayout title="Experiment #001">
+  <h2>Hello World!</h2>
+  
+  <section class="introduction">
+    <p>The first experiment focuses on developing a template/layout for future lab experiments.</p>
+  </section>
+  
+  <section class="input">
+    <h3>Input</h3>
+    <input bind:value={name}/>
+  </section>
+  
+  <section class="output">
+    <h3>Output</h3>
+    <p>Hello {name}!</p>
+  </section>
+</LabLayout>
