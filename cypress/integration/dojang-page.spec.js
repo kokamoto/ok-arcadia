ec7430da63ae57f326096d02531ccb145ac2ee94
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2022-03-01 18:18:59 -0800

    Adding sub-sections to USA Poomsae Ranking page

--- cypress/integration/dojang-page.spec.js
+++ cypress/integration/dojang-page.spec.js
@@ -6,15 +6,25 @@ describe('The Dojang Page', () => {
     cy.get('title').should('contain', 'OK Arcadia: The Dojang');
   });
 
-  it('should be able to navigate to the USA Taekwondo Poomsae rankings via link', () => {
+  it('should be able to navigate to the USA Taekwondo Poomsae Rankings via link', () => {
     cy.visit('/dojang');
     cy.get('a#poomsae-rankings-usa').click();
     cy.url().should('include', '/dojang/poomsae-rankings/usa')
   });
 
-  it('should be able to navigate to the USA Taekwondo Poomsae rankings via URL', () => {
+  it('should be able to navigate to the USA Taekwondo Poomsae Rankings via URL', () => {
     cy.visit('/dojang/poomsae-rankings/usa');
     cy.get('h1').should('contain', 'The US National Poomsae Rankings');
     cy.get('title').should('contain', 'OK Arcadia: The Dojang');
+  });
+
+  it('should have a Recognized Poomsae section in the USA Taekwondo Poomsae Rankings page', () => {
+    cy.visit('/dojang/poomsae-rankings/usa');
+    cy.contains('Recognized Poomsae').should('be.visible');
+  });
+
+  it('should have a Freestyle Poomsae section in the USA Taekwondo Poomsae Rankings page', () => {
+    cy.visit('/dojang/poomsae-rankings/usa');
+    cy.contains('Freestyle Poomsae').should('be.visible');
   });
 });
--- src/routes/dojang/poomsae-rankings/usa.svelte
+++ src/routes/dojang/poomsae-rankings/usa.svelte
@@ -3,3 +3,7 @@
 </svelte:head>
 
 <h1 class="title">The US National Poomsae Rankings</h1>
+
+<h2 class="subtitle">Recognized Poomsae</h2>
+
+<h2 class="subtitle">Freestyle Poomsae</h2>
