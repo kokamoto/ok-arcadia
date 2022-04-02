8a076c9707cafd9561c5b0afebe8db2346aa410b
Kevin Okamoto <kevin.okamoto@sap.com>
2022-04-01 19:13:29 -0700

    Adding Collection layouts

---
+++ src/lib/components/collections/CardCollection.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let items;
+</script>
+<div class="columns is-multiline">
+{#each items as item}
+<div class="column is-one-quarter">
+  <div class="box">
+  <slot prop={item}></slot>
+  </div>
+</div>
+{/each}
+</div>
