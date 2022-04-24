647025c9e32d9c2e87090a7d2b38ada7ed299e23
kokamoto <kevin.k.okamoto@gmail.com>
2022-04-24 11:14:55 -0700

    Added CSS styles to RankingListItem

--- src/lib/components/app/dojang/RankingListItem.svelte
+++ src/lib/components/app/dojang/RankingListItem.svelte
@@ -5,16 +5,48 @@
   export let rank = undefined;
 </script>
 
-<div class="box mb-0">
-  <article class="media">
-    <div class="media-left">
+<div class="box">
+  <article class="ranking">
+    <div class="rank">
       {rank}
     </div>
-    <div class="media-content has-text-weight-bold">
+    <div class="name">
       {name}
     </div>
-    <div class="media-right">
+    <div class="points">
       {points}
     </div>
   </article>
 </div>
+
+<style lang="scss">
+  .box {
+    border: 1px solid #333;
+    border-radius: 5px;
+    margin-bottom: 1rem;
+    line-height: 3rem;
+  }
+  .ranking {
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+    background-color: #eee;
+  }
+  .rank {
+    text-align: center;
+    height: 2.5rem;
+    width: 2.5rem;
+    background-color: #fff;
+    border: 1px solid #eee;
+    border-radius: 1.25rem;
+    line-height: 2.5rem;
+    margin-left: 0.25rem;
+  }
+  .name {
+    padding-left: 0.5rem;
+    flex: 1;
+  }
+  .points {
+    padding-right: 0.5rem;
+  }
+</style>
