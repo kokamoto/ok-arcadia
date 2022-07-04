0d87ef9aa0804703dbae6ed971f5df5c9b0f2356
kokamoto <kevin.k.okamoto@gmail.com>
2022-07-04 06:18:47 -0700

    Initial GraphQL server code

--- server/README.md
+++ server/README.md
@@ -1,3 +1,6 @@
 # OK Arcadia Server
 
 TBD
+
+# References
+* [Apollo GraphQL Server](https://www.apollographql.com/docs/apollo-server)
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Recipe {
+    title: String!
+    description: String
+  }
+
+  type Query {
+    recipes: [Recipe]
+  }
+`;
+
+const recipes = [{
+  title: 'Gluten-free Chocolate Chip Cookies',
+  description: 'Warm gluten-free chocolate chip cookies are yummy.'
+}, {
+  title: 'Chicken Soup',
+  description: 'Soup for your soul.'
+}];
+
+const resolvers = {
+  Query: {
+    recipes: () => recipes,
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded'
+});
+
+server.listen().then(({url}) => {
+  console.log(`Server is ready at ${url}`);
+});
